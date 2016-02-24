ac4fd25718a500ff781d8485c2dbf0dfbca3d90a
christophd <deppisch@consol.de>
2016-02-24 23:05:42 +0100

    Show test action details on mouse over

--- citrus-admin-client/src/js/components/design/test.action.component.ts
+++ citrus-admin-client/src/js/components/design/test.action.component.ts
@@ -3,16 +3,26 @@ import {TestAction} from "../../model/tests";
 
 @Component({
     selector: "test-action",
-    template: '<div class="test-action" (click)="select()">' +
-    '<i class="fa icon-{{action.type}}"></i>' +
-    '<span>{{action.type}}</span>' +
-    '</div>'
+    template: `<div class="test-action" (click)="select()" (mouseenter)="focused = true" (mouseleave)="focused = false">
+            <i class="fa icon-{{action.type}}"></i>
+            <span>{{action.type}}</span>
+            <div [hidden]="!focused" class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">{{action.type}}</h3>
+              </div>
+              <div class="panel-body">
+                <p *ngFor="#property of action.properties">{{property.id}}={{property.value}}</p>
+              </div>
+            </div>
+        </div>`
 })
 export class TestActionComponent {
 
     @Input() action: TestAction;
 
     @Output() selected = new EventEmitter(true);
+
+    focused = false;
 
     constructor() {}
 
