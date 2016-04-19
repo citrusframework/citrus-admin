c2f531827ad78893fc79e95f853c0c48b82fb397
christophd <deppisch@consol.de>
2016-04-19 14:50:41 +0200

    Use hash location strategy and fixed typings install as postinstall script

--- citrus-admin-client/src/app/main.ts
+++ citrus-admin-client/src/app/main.ts
@@ -1,7 +1,8 @@
+import {provide}    from 'angular2/core'
 import {bootstrap}    from 'angular2/platform/browser'
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {AppComponent} from './components/app.component'
 import 'rxjs/Rx';
 
 //noinspection TypeScriptValidateTypes
-bootstrap( AppComponent, [ROUTER_PROVIDERS] );
+bootstrap( AppComponent, [ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})] );
