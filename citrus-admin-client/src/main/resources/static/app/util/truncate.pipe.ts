3ffd311636de1b91311cea0b876e717ca5234a1e
christophd <deppisch@consol.de>
2016-10-24 15:36:03 +0200

    #2 Fixed pipe after angular2 update

--- citrus-admin-client/src/main/resources/static/app/util/truncate.pipe.ts
+++ citrus-admin-client/src/main/resources/static/app/util/truncate.pipe.ts
@@ -1,11 +1,11 @@
-import {Pipe} from '@angular/core'
+import {Pipe, PipeTransform} from '@angular/core'
 
 @Pipe({
     name: 'truncate'
 })
-export class TruncatePipe {
-    transform(value: string, args: string[]) : string {
-        let limit = args.length > 0 ? parseInt(args[0], 10) : 10;
+export class TruncatePipe implements PipeTransform {
+    transform(value: string, ...args: any[]) : string {
+        let limit = args.length > 0 ? parseInt(args[0], 10) : 20;
         let trail = args.length > 1 ? args[1] : '...';
 
         return value.length > limit ? value.substring(0, limit) + trail : value;
