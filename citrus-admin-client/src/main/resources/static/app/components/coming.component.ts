bd53d0119b17006630dd477bd5c6eaa3f37177c5
timkeiner <keiner.tim@gmail.com>
2017-02-08 18:30:43 +0100

    hopefully cleaned all git related confusing

--- citrus-admin-client/src/main/resources/static/app/components/coming.component.ts
+++
@@ -1,6 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    templateUrl: 'app/components/coming-soon.html'
-})
-export class ComingComponent { }
--- citrus-admin-client/src/main/resources/static/app/components/header.component.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'header',
-    templateUrl: 'app/components/header.html'
-})
-export class HeaderComponent { }
