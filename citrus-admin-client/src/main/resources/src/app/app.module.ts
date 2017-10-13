5613870ad596d4e5647893875544b0734e33388a
christophd <deppisch@consol.de>
2017-10-13 14:09:40 +0200

    Update Angular versions - does not compile

--- citrus-admin-client/src/main/resources/src/app/app.module.ts
+++ citrus-admin-client/src/main/resources/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
